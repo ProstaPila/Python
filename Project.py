36fa7d06fde8cc0f93cdd37cbd104f0485fd757d
Piotr Lewandowski <piotrekm.lewan@gmail.com>
2025-08-05 20:21:38 +0200

    commit

--- Project.py
+++ Project.py
@@ -1,1 +1,30 @@
-print("Test")
+import time
+
+def temperatureConversion():
+    while True:
+        inputTemperature = input("Choose input temperature type [C,F,K] ->")
+        
+        match inputTemperature:
+            case "C":
+                break
+            case "F":
+                break
+            case "K":
+                break
+            case _:
+                print("\nChoose the valid option\n")
+                time.sleep(1)
+
+
+while True:
+    primaryChoice = input("Choose a number on a keyboard coresponding to the number of the function:\n 1. Temperature Conversion\n 0. Exit\n Choose option->")
+
+    match primaryChoice:
+        case "1":
+            temperatureConversion()
+            break
+        case "0":
+            break
+        case _:
+            print("\nChoose the valid option\n")
+            time.sleep(1)
